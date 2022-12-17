126ddcb6caf59886af6e7aa22c8e96841e318ee1
Mobid <118357977+MobidGenius@users.noreply.github.com>
2022-12-17 03:54:13 +0000

    Class Marksheet
    This Will Display Your Whole Class Result Combined in Tabular Form

--- MarkSheet Tool/FinalResult.py
+++ MarkSheet Tool/FinalResult.py
@@ -1,1 +1,6 @@
+#@title **CLASS MARK SHEET**
+#RUN THIS CELL TO GET THE OUTPUT
 
+from tabulate import tabulate
+
+print(tabulate(grandlist, headers=["NAME:","ROLL NO","MATHEMATICS","PHYSICS","CHEMISTRY","COMPUTER SCIENCE","ENGLISH","TOTAL","PERCENTAGE","PASS/FAIL"]))

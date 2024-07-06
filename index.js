4d71e831939a7171d6dbe90820467ca3b23e1fc3
Mobid <118357977+MdMobid@users.noreply.github.com>
2024-07-06 07:00:37 +0000

    js

---
+++ index.js
@@ -0,0 +1,30 @@
+const TelegramBot = require("node-telegram-bot-api");
+const axios = require("axios");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Bot is alive");
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", async (msg) => {
+    const chatId = msg.chat.id;
+    const userInput = msg.text;
+    const msg_id = msg.message_id;
+
+    sent_message = await bot.sendMessage(chatId, userInput, {
+        reply_to_message_id: msg_id,
+    });
+
+});
+
+module.exports = app;
